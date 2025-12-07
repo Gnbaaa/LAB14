81c382747e1ad43dc4a5c2f7c97365c26f5575bb
Ganbayar <bayasgalanganbayar98@gmail.com>
2025-12-07 18:12:08 +0800

    lab14

---
+++ src/test/java/lab14/sict/must/edu/mn/AppTest.java
@@ -0,0 +1,12 @@
+package lab14.sict.must.edu.mn;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
